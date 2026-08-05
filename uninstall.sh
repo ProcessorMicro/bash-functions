d8dcfd5b538bf4f0a775a5229933dc555b0b3cda
Mike <42617717+ProcessorMicro@users.noreply.github.com>
2026-08-05 16:17:01 -0600

    Add files via upload

--- uninstall.sh
+++ uninstall.sh
@@ -46,19 +46,21 @@ function Install() {
   Beginning installation..."
 
   mkdir -p "${BinDir}"				# Ensure it exists
-  [[ :${PATH}: =~ :${BinDir}: ]] || WARNING "\nYour search path \"PATH\" doesn't contain the path \"${BinDir}\".\nIt is required for \"functions.sh\" to operate correctly.\n"
+  if [[ :${PATH}: =~ :${BinDir}: || :${PATH}: =~ :$(echo ${BinDir}): ]] ; then
+    WARNING "\nYour search path \"PATH\" doesn't contain the path \"${BinDir}\".\nIt is required for \"functions.sh\" to operate correctly.\n"
+  fi
 
   VERIFY_REQUIREMENTS				# All the apps that are needed
 
   # /etc/profile.d/...
-  if [[ -f /etc/${DefaultsFile} ]] ; then
-    echo -e "The GET_ARGS global defaults file \"/etc/${DefaultsFile}\" exists\n    so it was not overwritten.\nManually merge the install file with your existing one.\nThe install version is in \"${WhereAmI}/${DefaultsFile}\"."
+  if [[ -f ${Etc}/${DefaultsFile} ]] ; then
+    echo -e "The GET_ARGS global defaults file \"${Etc}/${DefaultsFile}\" exists\n    so it was not overwritten.\nManually merge the install file with your existing one.\nThe install version is in \"${WhereAmI}/${DefaultsFile}\"."
   else
-    cp ${DefaultsFile} /etc/${DefaultsFile}  || ERROR "Copy of script \"${DefaultsFile}\" failed."
+    cp ${DefaultsFile} ${Etc}/${DefaultsFile} || ERROR "Copy of script \"${DefaultsFile}\" failed."
   fi
-  cp -f ${ExtraFunctions} /etc/${ExtraFunctions} || ERROR "Copy of script \"${ExtraFunctions}\" failed."
-  chown root:root /etc/${ExtraFunctions} /etc/${DefaultsFile}
-  chmod 644 /etc/${ExtraFunctions} /etc/${DefaultsFile}
+  cp -f ${ExtraFunctions} ${Etc}/${ExtraFunctions} || ERROR "Copy of script \"${ExtraFunctions}\" failed."
+  chown root:root ${Etc}/${ExtraFunctions} ${Etc}/${DefaultsFile}
+  chmod 644 ${Etc}/${ExtraFunctions} ${Etc}/${DefaultsFile}
 
   # /usr/local/bin/...
   cd bin
@@ -75,30 +77,47 @@ function UnInstall() {
   echo -e "Uninstall script for the \"functions.sh\" distribution.
   Beginning uninstall...\n"
 
-  read -p "Do you really want to uninstall the \"functions.sh\" distribution? " Ans
   [[ ${Ans,,} =~ ^y ]] || exit
-  rm -f "/etc/${DefaultsFile}"
-  rm -f "/etc/${ExtraFunctions}"
+  rm -f "${Etc}/${DefaultsFile}" || ERROR "while removing script \"${Etc}/${DefaultsFile}\"."
+  rm -f "${Etc}/${ExtraFunctions}" || ERROR "while removing script \"${Etc}/${ExtraFunctions}\"."
 
   cd bin
   while read -u 3 Script ; do
-    rm -f "${BinDir}/${Script}"
+    rm -f "${BinDir}/${Script}" || ERROR "while removing script \"${BinDir}/${Script}\"."
   done 3< <( find -type f )
 
   cd "${WhereAmI}/.."
   ( sleep 3; rm -rf "${WhereAmI}" ; echo -e "\nUninstall of \"functions.sh\" complete.\n" ) &
 }
 
-(( $( id -u ) )) && ERROR "You must run this script a \"root\"."
-
-BinDir="/usr/local/bin"				# Where we want to put the scripts
-DefaultsFile="profile.d/FUNCTIONS-SH-GLOBAL-DEFAULTS.sh"
-ExtraFunctions="profile.d/FUNCTIONS-SH-EXTRA-FUNCTIONS.sh"
-
+Ubf="useful-bash-functions"
 ThisIsMe="$(realpath "${BASH_SOURCE[0]}")"	# Path of this script
 WhoAmI="${ThisIsMe##*/}"			# Basename of this script
+WhoAmIaction="${WhoAmI%.*}"
 WhereAmI="${ThisIsMe%/*}"			# DirName of this script
 cd "${WhereAmI}"				# Go there
+
+DefaultsFile="FUNCTIONS-SH-GLOBAL-DEFAULTS.sh"
+ExtraFunctions="FUNCTIONS-SH-EXTRA-FUNCTIONS.sh"
+ForWho="$( id -u -n )"
+
+if [[ $1 =~ -h ]] ; then
+  echo -e "${WhoAmIaction^} the \"${Ubf}\" distribution.\nUsage: ${WhoAmI} [--system-install]\nWhere:\n  --system-install\n    ${WhoAmIaction^} as a system-wide appplication.\n    The default is to ${WhoAmIaction} it for only this user: ${ForWho}"
+  exit
+elif [[ $1 == --system-${WhoAmIaction} ]] ; then
+  (( $( id -u ) )) && ERROR "You must run this script as \"root\"."
+  BinDir="/usr/local/bin"			# Where we want to put the system scripts
+  EtcDir="/etc"					# Where we want to put the system scripts
+  DefaultsFile="profile.d/${DefaultsFile}"
+  ExtraFunctions="profile.d/${ExtraFunctions}"
+  ForWho="root"
+else
+  BinDir="~/bin"				# Where we want to put the user scripts
+  EtcDir="${BinDir}"				# Where we want to put the user scripts
+fi
+
+read -p "Do you really want to ${WhoAmIaction} ${Ubf} (y, n)? " Ans
+[[ ${Ans,,} == y ]] || exit
 
 if [[ ${WhoAmI} == install.sh ]] ; then
   Install
